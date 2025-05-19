67910116b0e9260a0f41bb7c21be3d945842e6be
Brandon <brandon_smith3@icloud.com>
2025-05-18 23:38:00 -0700

    updated

---
+++ Lesson6/main.py
@@ -0,0 +1,74 @@
+'''
+start a virtual enviroment
+pip install pillow
+pip install requests
+On windows, tkinter is installed by default
+'''
+
+import requests
+import io # needed to open image from URL and convert from raw bytes
+from pathlib import Path # if you want to create a cache of sprites
+import tkinter as tk
+from PIL import Image, ImageTk
+import random
+
+
+# ---------- constants ----------
+POKE_COUNT = 1025                              # adjust if new pokemon come out (is this a thing?)
+API_URL    = "https://pokeapi.co/api/v2/pokemon/{}" # we will use {} later for substitution with .format()
+POKE_ID = random.randint(0,POKE_COUNT)
+
+# ---------- create our own dictionary of pokemon info ----------
+
+request = requests.get(API_URL.format(POKE_ID),timeout=10)
+request.raise_for_status()
+data = request.json()
+formatted_data = {
+    "id":           POKE_ID,
+    "name":         data["name"].title(),
+    "types":        [t["type"]["name"] for t in data["types"]], 
+    "weight_kg":    data["weight"]/10,
+    "sprite":       data["sprites"]["front_default"]
+    
+}
+
+# ---------- GUI Stuff ----------
+
+def show_pokemon():
+    try:
+        root.img_label.photo = tk_image
+        root.img_label.config(image=tk_image)
+
+        types = " / ".join(formatted_data["types"]).title() # convert dict to string
+        root.info_label.config(
+            text=f"{formatted_data['name']}  (#{POKE_ID})\n"
+                    f"Type: {types}\n"
+                    f"Weight: {formatted_data['weight_kg']} kg"
+        )
+    except Exception as err:
+        root.info_label.config(text=f"Error: {err}")
+        
+
+root = tk.Tk()
+root.title("Random Pokémon Generator")
+root.geometry("300x300")
+root.resizable(False, False)
+
+# widgets
+root.img_label = tk.Label()
+root.img_label.pack(pady=(10, 5))
+root.info_label = tk.Label(font=("Helvetica", 12), justify="center")
+root.info_label.pack(pady=(0, 10))
+
+root.btn = tk.Button(
+    text="Show Pokémon",
+    font=("Helvetica", 14, "bold"),
+    command = show_pokemon
+)
+root.btn.pack(fill="x", padx=20, pady=5)
+
+img_bytes = requests.get(formatted_data["sprite"],timeout=10).content
+pillow_image = Image.open(io.BytesIO(img_bytes)).resize((200,200))
+tk_image = ImageTk.PhotoImage(pillow_image)
+
+root.mainloop()
